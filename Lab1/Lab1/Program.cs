45f03d8aa4290dbba4a4b5a4e75ff696c7291d0c
Wiktoria Banaszak <wbanaszak@edu.cdv.pl>
2025-10-11 13:38:55 +0200

    meowzadania

--- Lab1/Lab1/Program.cs
+++ Lab1/Lab1/Program.cs
@@ -1,3 +1,63 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿ const int requiredAge = 18;
+const string accessDeniedMessage = "Musisz mieć 18 lat.";
+const string accessAllowedMessage = "Witamy w sklepie.";
+const string accessRestrictedMessage = "Masz ograniczony dostęp do produktów.";
+int age;
+int restrictedAge = 14;
+do
+{
+    Console.WriteLine("Wprowadź wiek:");
+    string input = Console.ReadLine();
 
-Console.WriteLine("Hello, World!");
+    age = Convert.ToInt32(input);
+
+    if (age >= requiredAge)
+    {
+        Console.WriteLine(accessAllowedMessage);
+    }
+    else if (age >= restrictedAge)
+    {
+        Console.WriteLine($"{accessAllowedMessage} {accessRestrictedMessage}");
+    }
+    else
+    {
+        Console.WriteLine(accessDeniedMessage);
+    }
+} while (age <= restrictedAge); 
+
+/*
+// zad. 1
+string password = "admin123";
+string input;
+Console.WriteLine("Wprowadź hasło:");
+input = Console.ReadLine();
+
+while (input != password)
+{
+    Console.WriteLine("Spróbuj ponownie:");
+    input = Console.ReadLine();
+}
+
+Console.WriteLine("Witamy w systemie.");
+*/
+
+/*
+// zad. 2
+int liczba;
+do
+{
+    Console.WriteLine("Podaj liczbę większą od 0:");
+    string input = Console.ReadLine();
+    liczba = Convert.ToInt32(input);
+} while (liczba ! > 0);
+Console.WriteLine("Dziękuje.");
+*/
+
+// zad. 3
+/*
+string[] miasta = { "Szczecin", "Poznań", "Warszawa", "Gdańsk", "Zakopane" };
+foreach (string miasto in miasta)
+{
+    Console.WriteLine(miasto);
+}
+*/
