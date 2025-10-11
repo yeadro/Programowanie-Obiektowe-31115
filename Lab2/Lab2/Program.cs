fdee21a1778cf16d657281ca6da5e65f03943493
Wiktoria Banaszak <wbanaszak@edu.cdv.pl>
2025-10-11 14:22:38 +0200

    zzz

---
+++ Lab2/Lab2/Program.cs
@@ -0,0 +1,27 @@
+﻿/*
+class Osoba
+{
+    public string Imie;
+    public int Wiek;
+
+    public void PrzedstawSie()
+    {
+        Console.WriteLine($"Mam na imię {Imie} i mam {Wiek} lat.");
+    }
+
+    static void Main(string[] args)
+    {
+        Osoba Jan = new Osoba();
+        Jan.Imie = "Jan";
+        Jan.Wiek = 20;
+        Jan.PrzedstawSie();
+        
+        Osoba Anna = new Osoba();
+        Anna.Imie = "Anna";
+        Anna.Wiek = 25;
+        Anna.PrzedstawSie();
+    }
+}
+*/
+
+
